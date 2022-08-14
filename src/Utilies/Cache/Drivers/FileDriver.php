6bdc8bef7b6a3ad3e092eadca88d4ad2b8a91946
ghaninia <mohamadaminghaninia@gmail.com>
2022-08-14 17:21:55 +0430

    feature:add file info

--- src/Utilies/Cache/Drivers/FileDriver.php
+++ src/Utilies/Cache/Drivers/FileDriver.php
@@ -9,76 +9,26 @@ class FileDriver implements CacheDriverInterface
 {
 
     /**
-     * @param string $key
-     * @return false|string
+     * get file path info
+     * @param string $path
+     * @return \stdClass
      */
-    public function get(string $key)
+    private function info(string $path)
     {
-        $file = $this->has($key);
-        return $file ? file_get_contents($file) : false ;
-    }
+        $information = new \stdClass() ;
+        $information->filectime = filectime($path) ;
+        $information->filemtime = filemtime($path) ;
+        $information->basename  = basename($path) ;
+        $information->mime  = mime_content_type($path) ;
 
-    /**
-     * @param string $key
-     * @param string $value
-     * @return bool
-     */
-    public function set(string $key, string $value): bool
-    {
-
-//        file_put_contents( ,$value) ;
-    }
-
-    public function generatePath(string $key)
-    {
-        $file = $this->has($key) ;
-
-    }
-
-    /**
-     * Delete files that have expired
-     *
-     * @return int
-     */
-    public function forgetExpired(): int
-    {
-        $path[] = $this->getBaseDir() ;
-        $path[] = "*" ;
-        $counter = 0 ;
-
-        $files = implode( DIRECTORY_SEPARATOR , $path);
-
-        foreach ( glob($files , GLOB_BRACE) as $file) {
-            if($this->isExpired($file)){
-                unlink($file) ;
-                $counter++ ;
-            }
-        }
-
-        return $counter ;
-    }
-
-    /**
-     * search cache file path by key name
-     * @param string $name
-     * @return false|string
-     */
-    protected function has(string $name)
-    {
-        $path[] = $this->getBaseDir();
-        $path[] = $name;
-        $path[] = '*';
-        $path = implode(DIRECTORY_SEPARATOR, $path);
-
-        $files = glob($path, GLOB_BRACE);
-        return count($files) ? $files[0] : false ;
+        return $information;
     }
 
     /**
      * get cache directory path
      * @return string
      */
-    protected function getBaseDir(): string
+    protected function sysBaseDir(): string
     {
         return config("interactive.driver.file.cache_dir", storage_path("interactive"));
     }
@@ -87,7 +37,7 @@ class FileDriver implements CacheDriverInterface
      * get cache directory path
      * @return string
      */
-    protected function getPrefix(): string
+    protected function sysPrefix(): string
     {
         return config("interactive.driver.file.prefix", "txt") ;
     }
@@ -96,39 +46,8 @@ class FileDriver implements CacheDriverInterface
      * get ttl expire at
      * @return int
      */
-    protected function getTTL(){
+    protected function sysTTL(){
         $ttl = config("interactive.cache.drivers.file.ttl", 3600) ;
         return (new \DateTime("+ {$ttl} seconds"))->getTimestamp();
     }
-
-    /**
-     * get path describe
-     * @param string $path
-     * @return \stdClass
-     */
-    protected function getDetail(string $path)
-    {
-        $basePath = basename( $path , $prefix = $this->getPrefix()) ;
-        $basePath = explode("_" , $basePath) ;
-
-        $stdClass = new \stdClass() ;
-        $stdClass->path = $path ;
-        $stdClass->prefix = $prefix ;
-        $stdClass->name = $basePath[0] ;
-        $stdClass->ttl = $basePath[1] ?? 0 ;
-
-        return $stdClass ;
-    }
-
-    /**
-     * Do it expire path
-     * @param string $path
-     * @return bool
-     */
-    protected function isExpired(string $path):bool
-    {
-        $detial = $this->getDetail($path) ;
-        return $detial->ttl < time() ;
-    }
-
 }
